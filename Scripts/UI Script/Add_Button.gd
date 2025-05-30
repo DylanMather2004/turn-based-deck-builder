e1e1407c8c44ce0a741050a79af68829ef5f18ec
Dylan Mather <116815437+DylanMather2004@users.noreply.github.com>
2025-05-30 10:14:58 +0100

    Updated Buttons
    Added the shrink and grow effect to all buttons

--- Scripts/UI Script/Add_Button.gd
+++ Scripts/UI Script/Add_Button.gd
@@ -1,9 +1,11 @@
 extends Node
 var card:Card
 var menu_manager
+@export var pressed_sound:WwiseEvent
 
 func _on_pressed() -> void:
 	menu_manager.add_card_to_deck(card)
+	pressed_sound.post(self)
 
 
 func _on_mouse_entered() -> void:
--- Scripts/UI Script/remove_card_button.gd
+++ Scripts/UI Script/remove_card_button.gd
@@ -1,7 +1,8 @@
 extends TextureButton
 var menu_manager
 var card:Card
-
+@export var pressed_sound:WwiseEvent
 
 func _on_pressed() -> void:
 	menu_manager.remove_card_from_deck(self)
+	pressed_sound.post(self)
